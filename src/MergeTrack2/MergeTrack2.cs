61701d2124cdaafa3d83b6bc722207c43859aea9
Zeno Gantner <zeno.gantner@gmail.com>
2011-03-29 15:08:53 +0200

    add merge feature for track 2

--- src/MergeTrack2/MergeTrack2.cs
+++ src/MergeTrack2/MergeTrack2.cs
@@ -1,20 +1,24 @@
 // Copyright (C) 2011 Zeno Gantner
-// 
+//
 // This file is part of MyMediaLite.
-// 
+//
 // MyMediaLite is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // MyMediaLite is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU General Public License
 //  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 
 class MegeTrack2
 {
@@ -22,13 +26,15 @@ class MegeTrack2
 	/// <param name="args">the command-line arguments</param>
 	public static void Main(string[] args)
 	{
+		int NUM_CANDIDATES = 6;
+
 		var ni = new NumberFormatInfo();
 		ni.NumberDecimalDigits = '.';
-				
+
 		// TODO check args.Length
-		
+
 		double weight_sum = 0;
-		
+
 		// parse command-line parameters
 		int num_files = int.Parse(args[0]);
 		var weights = new double[num_files];
@@ -40,20 +46,41 @@ class MegeTrack2
 			files[i]   = args[i + num_files + 1];
 		}
 		string output_filename = args[2 * num_files + 1];
-		
+
 		// open files
 		var readers = new BinaryReader[num_files];
 		for (int i = 0; i < num_files; i++)
 			readers[i] = new BinaryReader(new FileStream(files[i], FileMode.Open, FileAccess.Read));
-		
-		// create writer
-		var writer = new BinaryWriter(new FileStream(output_filename, FileMode.Create));
-		
-		// read, merge, and write
-		byte[][] ratings = new byte[num_files][];
-		while ( (ratings[0] = readers[0].ReadBytes(6)).Length > 0)
-		{
-			// TODO
-		}
+
+		using (var writer = new StreamWriter(output_filename))
+			try
+			{
+				// read, merge, and write
+				var votes = new char[num_files][];
+				while ( (votes[0] = readers[0].ReadChars(NUM_CANDIDATES)).Length > 0 )
+				{
+					for (int i = 1; i < readers.Length; i++)
+						votes[i] = readers[i].ReadChars(NUM_CANDIDATES);
+
+					var weighted_votes = new double[NUM_CANDIDATES];
+					for (int i = 0; i < readers.Length; i++)
+						for (int j = 0; j < NUM_CANDIDATES; j++)
+							if (votes[i][j] == '1')
+								weighted_votes[j] += weights[i];
+
+					var positions = new List<int>(new int[] { 0, 1, 2, 3, 4, 5 });
+					positions.Sort(delegate(int pos1, int pos2) { return weighted_votes[pos2].CompareTo(weighted_votes[pos1]); } );
+
+					for (int i = 0; i < NUM_CANDIDATES; i++)
+						if (positions.IndexOf(i) < 3)
+							writer.Write("1");
+						else
+							writer.Write("0");
+				}
+			}
+			catch (EndOfStreamException)
+			{
+				// do nothing
+			}
 	}
 }
