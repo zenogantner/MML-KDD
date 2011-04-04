a93b5ed163cee3f3830ee3c97287f903f3d9ca3a
Zeno Gantner <zeno.gantner@gmail.com>
2011-04-04 16:24:02 +0200

    fix KDD Cup scripts

--- examples/kddcup2011/track1.py
+++ examples/kddcup2011/track1.py
@@ -5,14 +5,17 @@ clr.AddReference("MyMediaLite.dll")
 clr.AddReference("MyMediaLiteExperimental.dll")
 from MyMediaLite import *
 
+num_train_ratings = 11696
+num_validation_ratings = 220
+num_test_ratings = 308
 train_file      = "trainIdx1.firstLines.txt"
 validation_file = "validationIdx1.firstLines.txt"
 test_file       = "testIdx1.firstLines.txt"
 
 # load the data
-training_data   = IO.KDDCup2011.Ratings.Read(train_file)
-validation_data = IO.KDDCup2011.Ratings.Read(validation_file)
-test_data       = IO.KDDCup2011.Ratings.ReadTest(test_file)
+training_data   = IO.KDDCup2011.Ratings.Read(train_file, num_train_ratings)
+validation_data = IO.KDDCup2011.Ratings.Read(validation_file, num_validation_ratings)
+test_data       = IO.KDDCup2011.Ratings.ReadTest(test_file, num_test_ratings)
 
 item_relations = IO.KDDCup2011.Items.Read("trackData1.txt", "albumData1.txt", "artistData1.txt", "genreData1.txt", 1);
 print item_relations
