5dcbfa503baa9450f32655f4edee93edb7dacbfa
Zeno Gantner <zeno.gantner@gmail.com>
2011-04-06 15:52:48 +0200

    BPRMF: rm diagnostic output

--- src/MyMediaLite/ItemRecommendation/BPRMF.cs
+++ src/MyMediaLite/ItemRecommendation/BPRMF.cs
@@ -110,11 +110,6 @@ namespace MyMediaLite.ItemRecommendation
 			{
 				SampleTriple(out user_id, out item_id_1, out item_id_2);
 				UpdateFactors(user_id, item_id_1, item_id_2, true, true, true);
-
-				if (i % 10000 == 9999)
-					Console.Error.Write(".");
-				if (i % 200000 == 199999)
-					Console.Error.WriteLine();
 			}
 		}
 
