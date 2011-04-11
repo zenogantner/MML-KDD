f6572ab51668002b5d4184056bbcb1e452062647
Zeno Gantner <zeno.gantner@gmail.com>
2011-04-11 11:07:40 +0200

    track2: more flexible merge

--- src/MergeTrack2/MergeTrack2.cs
+++ src/MergeTrack2/MergeTrack2.cs
@@ -21,17 +21,14 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.IO;
+using MyMediaLite.Eval;
 using MyMediaLite.IO.KDDCup2011;
 
 class MergeTrack2
 {
 	const int NUM_CANDIDATES = 6;
 	
-	static double weight_sum = 0;
-    static List<double> weights = new List<double>();
-	static List<string> files   = new List<string>();
-	
-	static string output_filename;
+	static string validation_hits_file;
 	
 	/// <summary>Parameters: num_files weight_1 .. weight_n file_1 .. file_n output_file</summary>
 	/// <param name="args">the command-line arguments</param>
@@ -41,66 +38,87 @@ class MergeTrack2
 		ni.NumberDecimalDigits = '.';
 
 		// parse command-line parameters
-		bool find_best_number = args[0] == "find_best_number";
-		for (int i = find_best_number ? 1 : 0; i < args.Length - 1; i++)
+		string output_file = null;
+		//static bool find_best;
+	   	var p = new OptionSet() {
+   			{ "validation-hits-file=", v => validation_hits_file = v },
+			{ "output-file=",          v => output_file = v },
+   		  	//{ "find-best",             v => find_best = v != null },
+   	  	};
+   		IList<string> extra_args = p.Parse(args);
+
+		List<string> files = new List<string>();
+		List<double> weights = new List<double>();
+		foreach (string arg in extra_args)
 		{
-			string[] tokens = args[i].Split(':');
+			string[] tokens = arg.Split(':');
 						
 			if (tokens.Length == 2)
 				weights.Add(double.Parse(tokens[1]));
 			else
 				weights.Add(1);
-			weight_sum += weights.Last();
 			
 			files.Add(tokens[0]);
 		}
-		output_filename = args.Last();
 	
-		EvalFiles();
-		MergeFiles();
+		IList<byte> final_prediction      = MergeFiles(files, weights);
+		//IList<byte> validation_prediction = MergeFiles();
+		//EvalFiles();
+		WritePredictions(final_prediction, output_file);
 	}
 	
 	static void EvalFiles()
 	{
-		//Track2Items.Read(validation_hits_file);
+		Track2Items.Read(validation_hits_file);
+		//KDDCup.EvaluateTrack2();
 	}
 	
-	static void MergeFiles()
+	static IList<byte> MergeFiles(IList<string> files, IList<double> weights)
 	{
 		// open files
 		var readers = new BinaryReader[files.Count];
 		for (int i = 0; i < files.Count; i++)
 			readers[i] = new BinaryReader(new FileStream(files[i], FileMode.Open, FileAccess.Read));
-
-		using (var writer = new StreamWriter(output_filename))
-			try
+		
+		var combined_predictions = new List<byte>();
+		
+		try
+		{
+			// read and merge
+			var votes = new char[files.Count][];
+			while ( (votes[0] = readers[0].ReadChars(NUM_CANDIDATES)).Length > 0 )
 			{
-				// read, merge, and write
-				var votes = new char[files.Count][];
-				while ( (votes[0] = readers[0].ReadChars(NUM_CANDIDATES)).Length > 0 )
-				{
-					for (int i = 1; i < readers.Length; i++)
-						votes[i] = readers[i].ReadChars(NUM_CANDIDATES);
+				for (int i = 1; i < readers.Length; i++)
+					votes[i] = readers[i].ReadChars(NUM_CANDIDATES);
 
-					var weighted_votes = new double[NUM_CANDIDATES];
-					for (int i = 0; i < readers.Length; i++)
-						for (int j = 0; j < NUM_CANDIDATES; j++)
-							if (votes[i][j] == '1')
-								weighted_votes[j] += weights[i];
+				var weighted_votes = new double[NUM_CANDIDATES];
+				for (int i = 0; i < readers.Length; i++)
+					for (int j = 0; j < NUM_CANDIDATES; j++)
+						if (votes[i][j] == '1')
+							weighted_votes[j] += weights[i];
 
-					var positions = new List<int>(new int[] { 0, 1, 2, 3, 4, 5 });
-					positions.Sort(delegate(int pos1, int pos2) { return weighted_votes[pos2].CompareTo(weighted_votes[pos1]); } );
+				var positions = new List<int>(new int[] { 0, 1, 2, 3, 4, 5 });
+				positions.Sort(delegate(int pos1, int pos2) { return weighted_votes[pos2].CompareTo(weighted_votes[pos1]); } );
 
-					for (int i = 0; i < NUM_CANDIDATES; i++)
-						if (positions.IndexOf(i) < 3)
-							writer.Write("1");
-						else
-							writer.Write("0");
-				}
+				for (int i = 0; i < NUM_CANDIDATES; i++)
+					if (positions.IndexOf(i) < 3)
+						combined_predictions.Add(1);
+					else
+						combined_predictions.Add(0);
 			}
-			catch (EndOfStreamException)
-			{
-				// do nothing
-			}		
+		}
+		catch (EndOfStreamException)
+		{
+			// do nothing
+		}
+		
+		return combined_predictions;
 	}
+	
+	static void WritePredictions(IList<byte> predictions, string output_file)
+	{
+		using (var writer = new StreamWriter(output_file))
+			foreach (byte p in predictions)
+				writer.Write(p);
+	}	
 }
