eff5a0b255f9d4c5fd84f7e08ccc1234fbf3dc6c
Zeno Gantner <zeno.gantner@gmail.com>
2011-04-28 18:09:20 +0200

    add BoldDriverBiasedMatrixFactorization

---
+++ src/MyMediaLiteExperimental/RatingPrediction/BoldDriverBiasedMatrixFactorization.cs
@@ -0,0 +1,57 @@
+// Copyright (C) 2011 Zeno Gantner
+// 
+// This file is part of MyMediaLite.
+// 
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+
+namespace MyMediaLite.RatingPrediction
+{
+	/// <summary>Biased matrix factorization with bold driver heuristic</summary>
+	/// <remarks>
+	/// See
+	/// Rainer Gemulla, Peter J. Haas, Erik Nijkamp, Yannis Sismanis:
+	/// Large-Scale Matrix Factorization with Distributed Stochastic Gradient Descent
+	/// </remarks>
+	public class BoldDriverBiasedMatrixFactorization : BiasedMatrixFactorization
+	{
+		double last_loss = double.NegativeInfinity;
+		
+		/// <inheritdoc/>		
+		protected override void InitModel()
+		{
+			base.InitModel();
+			last_loss = ComputeLoss();
+		}
+		
+		/// <inheritdoc/>		
+		public override void Iterate()
+		{
+			base.Iterate();
+			
+			double loss = ComputeLoss();
+					
+			if (loss > last_loss)
+				LearnRate *= 0.5;
+			else if (loss < last_loss)
+				LearnRate *= 1.05;
+			
+			last_loss = loss;
+			
+			Console.Error.Write("loss {0} learn_rate {1} ", loss, LearnRate);			
+		}
+	}
+}
+
