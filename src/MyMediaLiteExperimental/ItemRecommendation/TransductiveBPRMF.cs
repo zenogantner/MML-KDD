b2f8bb46b28fe2e8eab73f8ac5d25034a79bef47
Zeno Gantner <zeno.gantner@gmail.com>
2011-06-24 12:30:03 +0200

    rename semisupervised to transductive

---
+++ src/MyMediaLiteExperimental/ItemRecommendation/TransductiveBPRMF.cs
@@ -0,0 +1,113 @@
+// Copyright (C) 2011 Zeno Gantner
+//
+// This file is part of MyMediaLite.
+//
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace MyMediaLite.ItemRecommendation
+{
+	/// <summary>BPR variant that takes into account some test data for training</summary>
+	public class TransductiveBPRMF : BPRMF, ITransductiveItemRecommender
+	{
+		/// <summary>Set containing all test users</summary>
+		public HashSet<int> TestUsers { get; set; }
+
+		/// <summary>Set containing all test items</summary>
+		public HashSet<int> TestItems { get; set; }
+
+		/// <summary>Learn rate modifier for users/items not contained in the test set</summary>
+		/// <remarks>Should be less or equal 1.</remarks>
+		public double NonTestModifier { get; set; }
+
+		/// <summary>Default constructur</summary>
+		public TransductiveBPRMF()
+		{
+			NonTestModifier = 1;
+		}
+		
+		/// <inheritdoc/>
+		protected override void UpdateFactors(int u, int i, int j, bool update_u, bool update_i, bool update_j)
+		{
+			double x_uij = Predict(u, i) - Predict(u, j);
+
+			double one_over_one_plus_ex = 1 / (1 + Math.Exp(x_uij));
+
+			double learn_rate_u = learn_rate;
+			double learn_rate_i = learn_rate;
+			double learn_rate_j = learn_rate;
+
+			// TODO do this via sampling, not learn rate modification
+			if (! TestUsers.Contains(u))
+				learn_rate_u *= NonTestModifier;
+			if (! TestItems.Contains(i))
+				learn_rate_i *= NonTestModifier;
+			if (! TestItems.Contains(j))
+				learn_rate_j *= NonTestModifier;
+
+			// adjust bias terms
+			if (update_i)
+			{
+				double bias_update = one_over_one_plus_ex - BiasReg * item_bias[i];
+				item_bias[i] += learn_rate * bias_update;
+			}
+
+			if (update_j)
+			{
+				double bias_update = -one_over_one_plus_ex - BiasReg * item_bias[j];
+				item_bias[j] += learn_rate * bias_update;
+			}
+
+			// adjust factors
+			for (int f = 0; f < num_factors; f++)
+			{
+				double w_uf = user_factors[u, f];
+				double h_if = item_factors[i, f];
+				double h_jf = item_factors[j, f];
+
+				if (update_u)
+				{
+					double uf_update = (h_if - h_jf) * one_over_one_plus_ex - reg_u * w_uf;
+					user_factors[u, f] = w_uf + learn_rate_u * uf_update;
+				}
+
+				if (update_i)
+				{
+					double if_update = w_uf * one_over_one_plus_ex - reg_i * h_if;
+					item_factors[i, f] = h_if + learn_rate_i * if_update;
+				}
+
+				if (update_j)
+				{
+					double jf_update = -w_uf * one_over_one_plus_ex - reg_j * h_jf;
+					item_factors[j, f] = h_jf + learn_rate_j * jf_update;
+				}
+			}
+		}
+
+				/// <inheritdoc/>
+		public override string ToString()
+		{
+			var ni = new NumberFormatInfo();
+			ni.NumberDecimalDigits = '.';
+
+			return string.Format(ni, "SemiSupervisedBPRMF num_factors={0} bias_reg={1} reg_u={2} reg_i={3} reg_j={4} non_test_modifier={5} num_iter={6} learn_rate={7} fast_sampling_memory_limit={8} init_mean={9} init_stdev={10}",
+								 num_factors, BiasReg, reg_u, reg_i, reg_j, NonTestModifier, NumIter, LearnRate, fast_sampling_memory_limit, InitMean, InitStdev);
+		}		
+	}
+}
+
---
+++ src/MyMediaLiteExperimental/ItemRecommendation/TransductiveBPRMF_KDD.cs
@@ -0,0 +1,105 @@
+// Copyright (C) 2011 Zeno Gantner
+//
+// This file is part of MyMediaLite.
+//
+// MyMediaLite is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MyMediaLite is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MyMediaLite.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using MyMediaLite.Util;
+
+namespace MyMediaLite.ItemRecommendation
+{
+	/// <summary>Semi-supervised BPRMF with frequency-adjusted sampling, prototype for KDD Cup 2011</summary>
+	public class TransductiveBPRMF_KDD : TransductiveBPRMF
+	{
+		// TODO offer this data structure from Feedback
+		/// <summary>array of user IDs of positive user-item pairs</summary>
+		protected int[] users;
+		/// <summary>array of item IDs of positive user-item pairs</summary>
+		protected int[] items;
+
+		// TODO move up in hierarchy
+		/// <summary>Modification factor for the learning rate of users to weight them higher or lower than items</summary>
+		public double UserLearnRateModifier { get; set; }
+
+		/// <summary>Weight for test users</summary>
+		public double TestUserWeight { get; set; }
+
+		/// <summary>Default constructor</summary>
+		public TransductiveBPRMF_KDD()
+		{
+			UserLearnRateModifier = 1;
+		}
+
+		/// <inheritdoc/>
+		protected override void InitModel()
+		{
+			base.InitModel();
+
+			users = new int[Feedback.Count];
+			items = new int[Feedback.Count];
+
+			int index = 0;
+			foreach (int user_id in Feedback.UserMatrix.NonEmptyRowIDs)
+				foreach (int item_id in Feedback.UserMatrix[user_id])
+				{
+					users[index] = user_id;
+					items[index] = item_id;
+
+					index++;
+				}
+		}
+
+		/// <inheritdoc/>
+		public override void Train()
+		{
+			// prepare helper data structures for training
+			user_pos_items = new List<IList<int>>();
+			for (int u = 0; u < MaxUserID + 1; u++)
+				user_pos_items.Add(new List<int>(Feedback.UserMatrix[u]));
+
+			// suppress using user_neg_items in BPRMF
+			FastSamplingMemoryLimit = 0;
+
+			base.Train();
+		}
+
+		/// <inheritdoc/>
+		protected override void SampleTriple(out int u, out int i, out int j)
+		{
+			// sample user from positive user-item pairs
+			int index = random.Next(0, items.Length - 1);
+			u = users[index];
+			i = items[index];
+
+			// sample negative item
+			do
+				j = items[random.Next(0, items.Length - 1)];
+			while (Feedback.UserMatrix[u, j]);
+		}
+
+		/// <inheritdoc/>
+		public override string ToString()
+		{
+			var ni = new NumberFormatInfo();
+			ni.NumberDecimalDigits = '.';
+
+			return string.Format(ni, "SemiSupervisedBPRMF_KDD num_factors={0} bias_reg={1} reg_u={2} reg_i={3} reg_j={4} non_test_modifier={5} num_iter={6} learn_rate={7} init_mean={8} init_stdev={9}",
+								 num_factors, BiasReg, reg_u, reg_i, reg_j, NonTestModifier, NumIter, learn_rate, InitMean, InitStdev);
+		}
+	}
+}
+
